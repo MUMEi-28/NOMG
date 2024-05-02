598f145794634d1c3278326fe9c5f28ce07ec57e
Maki-03 <168610036+Maki-03@users.noreply.github.com>
2024-05-02 18:10:15 +0800

    small repair in Billing

--- NOMG/NOMG/Form7.vb
+++ NOMG/NOMG/Form7.vb
@@ -2,12 +2,13 @@
     Private Sub btnPay_Click(sender As Object, e As EventArgs) Handles btnPay.Click
         Dim Change As Integer
         Change = txtPayment.Text - frmBilling.txtTotal.Text
-        If frmBilling.txtTotal.Text > txtPayment.Text Then
-            MsgBox("Insufficient Amount", vbOKOnly + vbExclamation, "Error")
-            txtPayment.Clear()
+        If frmBilling.txtTotal.Text <= txtPayment.Text Then
+            MsgBox("Change: " + Change.ToString, vbOKOnly, "Thank you")
+            Me.Hide()
 
         Else
-            MsgBox("Change: " + Change.ToString, vbOKOnly, "Thank you")
+            MsgBox("Insufficient Amount", vbOKOnly + vbExclamation, "Error")
+            txtPayment.Clear()
         End If
     End Sub
 End Class
