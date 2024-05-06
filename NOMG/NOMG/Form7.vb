f77c8bc051ef7d20c507ddc0de9e464a94f91705
Mark Luis Jefferson Figueroa <168610036+Maki-03@users.noreply.github.com>
2024-05-07 02:45:37 +0800

    Very super  small update

--- NOMG/NOMG/Form7.vb
+++ NOMG/NOMG/Form7.vb
@@ -1,18 +1,25 @@
 ﻿Public Class frmPayment
     Private Sub btnPay_Click(sender As Object, e As EventArgs) Handles btnPay.Click
         Dim Change As Integer
-        Change = txtPayment.Text - frmBilling.txtTotal.Text
-        If frmBilling.txtTotal.Text <= txtPayment.Text Then
-            MsgBox("Change: " + Change.ToString, vbOKOnly, "Thank you")
-            Me.Hide()
-            frmBilling.Hide()
-            frmMain.Show()
-
-
+        Dim Payment As Integer
+        Dim Total As Integer
+        If Integer.TryParse(txtPayment.Text, Payment) AndAlso Integer.TryParse(frmBilling.txtTotal.Text, Total) Then
+            Change = Payment - Total
+            If Total <= Payment Then
+                MsgBox("Change: " & Change.ToString(), vbOKOnly, "Thank you")
+                Me.Hide()
+                frmBilling.Hide()
+                frmMain.Show()
+            Else
+                MsgBox("Insufficient Amount", vbOKOnly + vbExclamation, "Error")
+                txtPayment.Clear()
+            End If
         Else
-            MsgBox("Insufficient Amount", vbOKOnly + vbExclamation, "Error")
-            txtPayment.Clear()
+            MsgBox("Invalid payment amount or total", vbOKOnly + vbExclamation, "Error")
+
         End If
+
+
     End Sub
 
     Private Sub btnCancel_Click(sender As Object, e As EventArgs) Handles btnCancel.Click
