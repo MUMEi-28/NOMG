67d4ee2d246c566c932e336ac3fe16043bb0f976
Hannah Pulmano <hannah.pulmano865@gmail.com>
2024-05-16 06:40:31 +0800

    Hannah's update 29

--- NOMG/NOMG/frmAccountInformation.vb
+++ NOMG/NOMG/frmAccountInformation.vb
@@ -18,9 +18,9 @@ Public Class frmAccountInformation
 		Dim user1 As New User("Jose", "Tarlac", "jose@email", "1234", 24, "y", 0, listDoctors(0), Date.Today.Date)
 		Dim user2 As New User("Juana", "Pangasinan", "juana@email", "1234", 35, "y", 0, listDoctors(1), Date.Today.Date)
 		Dim user3 As New User("Cardo", "Davao", "cardo@email", "1234", 30, "n", 0, listDoctors(2), Date.Today.Date)
-		Dim user4 As New User("a", "a", "a", "a", 30, "n", 12, listDoctors(0), Date.Today.Date)
-		Dim user5 As New User("b", "b", "b", "b", 30, "n", 12, listDoctors(0), Date.Today.Date)
-		Dim user6 As New User("c", "c", "c", "c", 30, "n", 12, listDoctors(0), Date.Today.Date)
+		Dim user4 As New User("a", "a", "a", "a", 30, "n", 0, listDoctors(0), Date.Today.Date)
+		Dim user5 As New User("a", "b", "b", "b", 30, "n", 0, listDoctors(0), Date.Today.Date)
+		Dim user6 As New User("a", "c", "c", "c", 30, "n", 0, listDoctors(0), Date.Today.Date)
 
 		listUsers.Add(user1)
 		listUsers.Add(user2)
